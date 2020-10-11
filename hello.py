29fa44a21f4ee2f10eb84ac85c34fff7b5ff9e9b
amrita506 <72393948+amrita506@users.noreply.github.com>
2020-10-11 14:14:32 +0400

    lecture 3 python file

---
+++ hello.py
@@ -0,0 +1,23 @@
+#python 3 of python web programming
+'''name=input()
+print(f"Hello,{name}!")#format string
+'''
+#-----------------------------------------
+'''
+i=28
+print(f"i is {i}")
+f=2.8
+print(f"f is {f}")
+b=True
+print(f"b is {b}")
+n=None
+print(f"n is {n}")
+'''
+#----------------------------------------------
+x=20
+if x> 0:
+    print("x is positive")
+elif x<0:
+    print("x is negative")
+else:
+    print("x is zero")
