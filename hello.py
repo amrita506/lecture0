3db8c3eb9fe491f3150055d3b2ac8f1e794ee745
amrita506 <72393948+amrita506@users.noreply.github.com>
2020-10-12 17:32:31 +0400

    class added

--- hello.py
+++ hello.py
@@ -45,9 +45,11 @@ print(s)
 
 def square(x):
     return x*x
-for i in range(10):
-    print("{} squared is {}".format(i,square(i)))
-
+def main():
+    for i in range(10):
+        print("{} squared is {}".format(i,square(i)))
+if __name__=="__main__":
+    main()
 
 
 
